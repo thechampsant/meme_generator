ea63c3fbf7645ad206afbcfb8f267bc97a767a02
thechampsant <thechampsant@gmail.com>
2022-08-15 12:52:17 +0530

    Index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 9000
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function (req, res) {
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
